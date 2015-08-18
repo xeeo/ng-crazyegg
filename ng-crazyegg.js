d4bfd8b89e8796acda197bc9dfd456e13dbe5d18
Mark Daggett <mark@humansized.com>
2015-08-18 09:43:59 -0500

    Small improvements made: 1. Remove the reference to $q since it is unused 2. Standardized on spaces instead of tabs (previously both were used) 3. Removed dependency on the Angular-ui router, since the crazyegg loading process works just using the timeout

--- ng-crazyegg.js
+++ ng-crazyegg.js
@@ -1,42 +1,40 @@
+'use strict';
 angular.module('ng-crazyegg', ['ng'])
-	.factory('crazyegg', ['$rootScope', '$window', '$timeout',
-		function($rootScope, $window, $timeout, $q) {
-		    var service = $window.crazyegg = $window.crazyegg || {};
+  .factory('crazyegg', ['$rootScope', '$window', '$timeout',
+    function($rootScope, $window, $timeout) {
+      var service = $window.crazyegg = $window.crazyegg || {};
 
-		    service.loadProject = function(key) {
+      service.loadProject = function(key) {
 
-				/** remove previous script **/
-				var element = document.getElementById('crazyegg-js');
-				if (element) {
-					element.parentNode.removeChild(element);
-				}
+        /** remove previous script **/
+        var element = document.getElementById('crazyegg-js');
+        if (element) {
+          element.parentNode.removeChild(element);
+        }
 
-				/** create new script **/
-				key = key + '';
-				var scriptPath = key.substr(0, 4) + '/' + key.substr(4, 4);
+        /** create new script **/
+        key = key + '';
+        var scriptPath = key.substr(0, 4) + '/' + key.substr(4, 4);
 
-				var script = document.createElement('script');
-				script.type = 'text/javascript';
-				script.id = 'crazyegg-js';
-				script.async = true;
-				script.src = 'https://script.crazyegg.com/pages/scripts/' + scriptPath + '.js?' + (new Date().getTime());
-				script.onload = script.onreadystatechange = function () {};
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.id = 'crazyegg-js';
+        script.async = true;
+        script.src = 'https://script.crazyegg.com/pages/scripts/' + scriptPath + '.js?' + (new Date().getTime());
+        script.onload = script.onreadystatechange = function() {};
 
-				/** add new script **/
-				var first = document.getElementsByTagName('script')[0];
-				first.parentNode.insertBefore(script, first);
-		    };
+        /** add new script **/
+        var first = document.getElementsByTagName('script')[0];
+        first.parentNode.insertBefore(script, first);
+      };
 
-			service.initCrazyEgg = function(accountKey) {
-				$rootScope.$on('$stateChangeSuccess', function() {
-					$timeout(function() {
-						$window.crazyegg.loadProject(accountKey);
-					});
-				});
+      service.initCrazyEgg = function(accountKey) {
+        $timeout(function() {
+          $window.crazyegg.loadProject(accountKey);
+        });
+        return true;
+      };
 
-				return true;
-			};
-
-		    return service;
-		}
-	]);
+      return service;
+    }
+  ]);
